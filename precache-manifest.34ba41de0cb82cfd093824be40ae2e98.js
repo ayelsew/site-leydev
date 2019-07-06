d578ed6b001df74a6c77543c705c0ac22e1b0206
Ley_dev <wesley.waaraujo@gmail.com>
2019-07-05 23:40:21 -0300

    first commit

---
+++ precache-manifest.34ba41de0cb82cfd093824be40ae2e98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8828870a37b90fdd835cdf5319314c45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b91b12c1ea52dc026c42",
+    "url": "/static/css/main.98d8d49f.chunk.css"
+  },
+  {
+    "revision": "6e312ed54f00f91913e7",
+    "url": "/static/js/2.e6958039.chunk.js"
+  },
+  {
+    "revision": "b91b12c1ea52dc026c42",
+    "url": "/static/js/main.53bc01ba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
